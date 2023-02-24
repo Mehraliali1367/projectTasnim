c860a481edcabdc0a5470dcc8e4217996b4075e3
ali <sadadmehrali@gmail.com>
2023-02-25 00:24:13 +0330

    test img125

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -106,14 +106,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'amin',
-        # 'USER': 'amin',
-        # 'PASSWORD': 'amin**',
-        # 'HOST': 'db',
-        # 'PORT': '5432',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'amin',
+        'USER': 'amin',
+        'PASSWORD': 'amin**',
+        'HOST': 'db',
+        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
